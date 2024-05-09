183c96ab9a6c1b6503a97484add651896cb1f28b
truda <truda1999@gmail.com>
2024-05-09 06:14:00 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Muin(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в калькулятор!");
+
+            // Запрос ввода первого числа
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            // Запрос ввода оператора (+, -, *, /)
+            Console.Write("Введите оператор (+, -, *, /): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            // Запрос ввода второго числа
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            int result = 0;
+
+            // Выполнение операции в зависимости от введенного оператора
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: недопустимый оператор!");
+                    break;
+            }
+
+            // Вывод результата
+            Console.WriteLine($"Результат: {result}");
+
+            Console.WriteLine("Спасибо за использование калькулятора!");
+        }
+    }
+}
